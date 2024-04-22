65347f429e23c2ce28f599b49b1fba785f431161
JillRay973 <143532365+JillRay973@users.noreply.github.com>
2024-04-22 11:03:14 -0400

    Add files via upload

---
+++ WikipediaWebCrawl/src/WikipediaWebCrawler.java
@@ -0,0 +1,18 @@
+package src;
+
+public class WikipediaWebCrawler {
+
+    //BFS; Linked list since we don't know nodes ahead of time.
+    //for each of the links present in source file, make a thing that logs each of the pages that you get from
+    //clicking on that link. Proceed exponentially, branching out as a tree until the target page is found. Once that
+    //target is found, trace backwards to source and output length of path that is shortest. Node should go under first
+    //link to discover it because it is guaranteed to be the shortest path.
+
+
+//pages = nodes; links = edges
+
+    //six degrees of wikipedia website as framework inspo
+
+    //main function that takes in user input of targer and source via scanner
+
+}
